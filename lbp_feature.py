747fbf499a2bc3fe9c0d201f48092dad4c33392e
seancho1996 <seanchozzx@outlook.com>
2019-12-17 23:17:29 +0100

    modify lbp algo

--- lbp_feature.py
+++ lbp_feature.py
@@ -35,18 +35,39 @@ def cal_lbp(img_gs, radius, neighbours):
         for i in range(radius, img_gs.shape[0]-radius):
             for j in range(radius, img_gs.shape[1]-radius):
                 center_pix = img_gs[i, j]
-                current_gs = img_gs[i+x1, j+y1]*w1 + img_gs[i+x1, j+y2]*w2 + img_gs[i+x2, j+y1]*w3 + img_gs[i+x2, j+y2]*w4
-                dst[i-radius, j-radius] |= (current_gs > center_pix) << (neighbours-k-1)
+                current_gs = img_gs[i+x1, j+y1]*w1 + img_gs[i+x1, j+y2]*w2 + img_gs[i+x2, j+y1]*w3 + img_gs[i+x2, j+y2]*w4 # interpolation for the pixels on the circle
+                dst[i-radius, j-radius] |= (current_gs > center_pix) << (neighbours-k-1) # very tricky!! using bitwise operation to calculate the sum of all neighbours
+
     for i in range(dst.shape[0]):
         for j in range(dst.shape[1]):
             current_gs = dst[i, j]
             min_gs = current_gs
             for k in range(neighbours):
-                temp_gs = (current_gs >> (neighbours-k)) | (current_gs << k)
+                temp_gs = (current_gs >> (neighbours-k)) | (current_gs << k)  # again using bitwise operation to calculate the rotation-invariant LBP
                 if temp_gs < min_gs:
                     min_gs = temp_gs
             dst[i, j] = min_gs
     return dst
+
+def lbp_hist(img, piece_number):
+    stride1 = img.shape[0] // piece_number  # we cut the image into piece_number*piece_number pieces
+
+    # initialize the final LBP_histogram vector
+    final_lbp = np.array([])
+
+    # calculate the histogram LBP in every small piece
+    for i in range(piece_number):
+        for j in range(piece_number):
+            img_cut = img[i * stride1:(i + 1) * stride1, j * stride1:(j + 1) * stride1]
+            lbp_cut = cal_lbp(img_cut, 1, 8)
+            hist = np.zeros([256])
+            for m in range(lbp_cut.shape[0]):
+                for n in range(lbp_cut.shape[1]):
+                    hist[lbp_cut[m, n]] += 1
+            hist = hist / (lbp_cut.shape[0] * lbp_cut.shape[1])
+            final_lbp = np.append(final_lbp, hist)
+
+    return final_lbp
 
 def proc_main(O_IN):
     s_img_url_a = O_IN["s_img_url_a"]
@@ -58,22 +79,16 @@ def proc_main(O_IN):
     img_2 = cv.imread(s_img_url_b)
     img_gs2 = cv.cvtColor(img_2, cv.COLOR_BGR2GRAY)
 
-    dst1 = cal_lbp(img_gs1, 1, 8)
-    hist1 = np.zeros([256])
-    for i in range(dst1.shape[0]):
-        for j in range(dst1.shape[1]):
-            hist1[dst1[i, j]] += 1
-    hist1 = hist1 / (dst1.shape[0]*dst1.shape[1])
+    hist1 = lbp_hist(img_gs1, 4)  # here we divide the image into 4*4 = 16 pieces
+    hist1 = hist1 / 16  # normalization, in every small pices the sum = 1, so in total 16*1 = 16
     print("histogram_LBP input image:", hist1)
-    # print(sum(hist1))
+    print(np.array(hist1).shape)
+    print(sum(hist1))
 
-    dst2 = cal_lbp(img_gs2, 1, 8)
-    hist2 = np.zeros([256])
-    for i in range(dst2.shape[0]):
-        for j in range(dst2.shape[1]):
-            hist2[dst2[i, j]] += 1
-    hist2 = hist2 / (dst2.shape[0] * dst2.shape[1])
+    hist2 = lbp_hist(img_gs2, 4)
+    hist2 = hist2 / 16
     print("histogram_LBP generated image:", hist2)
+    print(np.array(hist2).shape)
     # print(sum(hist2))
 
     f_img_sim = 0
